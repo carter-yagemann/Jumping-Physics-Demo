c3c30345ada135cf71fc13d0afc577104011676c
Carter Yagemann <carter.yagemann@gmail.com>
2014-09-12 23:56:50 -0400

    Initial commit

---
+++ src/physicsdemo/PhysicsDemo.java
@@ -0,0 +1,20 @@
+// TODO add license
+
+package physicsdemo;
+
+/**
+ *
+ * @author Carter Yagemann
+ * 
+ * A simple demo of a "jump".
+ */
+public class PhysicsDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame.main(args);
+    }
+    
+}
